7561d014d2aeea1d67d589c0198c2d7e3902a2a9
marceltheshell <marcel.degas@gmail.com>
2018-06-25 22:45:10 -0700

    adds a post route for students in the controller

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,8 +1,32 @@
 class Api::StudentsController < ApplicationController
-  def index
-  end
 
   def create
-    binding.pry
+    student = Student.new(student_params)
+    if student.save
+		render json: {
+			status: 200, 
+			message: "successfully saved a student",
+			student: student
+		}.to_json
+	else
+		render json: {
+			status: 500, 
+			errors: list.errors
+		}.to_json
+	end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :gender)
   end
 end
+
+
+# curl \
+# -H "Accept: application/json" \
+# -H "Content-type: application/json" \
+# -X POST \
+# -d '{"first_name": "Marcel", "last_name": "Degas"}' \
+# http://localhost:3000/api/students
