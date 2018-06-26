8dabae3e1275d80c25d6debc159914c401a8daa2
marceltheshell <marcel.degas@gmail.com>
2018-06-25 19:29:21 -0700

    changes order of api directory

---
+++ app/controllers/api/students_controller.rb
@@ -0,0 +1,8 @@
+class Api::StudentsController < ApplicationController
+  def index
+  end
+
+  def create
+    binding.pry
+  end
+end
---
+++ spec/controllers/api/students_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Api::StudentsController, type: :controller do
+
+end
