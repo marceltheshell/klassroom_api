0108b34bd5430c7edcad294ad12b4b6ba4042fa6
marceltheshell <marcel.degas@gmail.com>
2018-06-25 22:54:32 -0700

    messes with api to make it work

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ The api for a project I developed to help teachers of English Language learners 
 
 * Ruby version "2.4.1"
 
+post /api/students
+# curl \
+# -H "Accept: application/json" \
+# -H "Content-type: application/json" \
+# -X POST \
+# -d '{"first_name": "Marcel", "last_name": "Degas"}' \
+# http://localhost:3000/api/students
+
--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,19 +1,19 @@
 class Api::StudentsController < ApplicationController
 
   def create
-    student = Student.new(student_params)
-    if student.save
+    student = Student.create!(student_params)
+    # if student.save
 		render json: {
 			status: 200, 
 			message: "successfully saved a student",
 			student: student
 		}.to_json
-	else
-		render json: {
-			status: 500, 
-			errors: list.errors
-		}.to_json
-	end
+	# else
+	# 	render json: {
+	# 		status: 500, 
+	# 		errors: list.errors
+	# 	}.to_json
+	# end
   end
 
   private
@@ -22,11 +22,3 @@ class Api::StudentsController < ApplicationController
     params.require(:student).permit(:first_name, :last_name, :gender)
   end
 end
-
-
-# curl \
-# -H "Accept: application/json" \
-# -H "Content-type: application/json" \
-# -X POST \
-# -d '{"first_name": "Marcel", "last_name": "Degas"}' \
-# http://localhost:3000/api/students
