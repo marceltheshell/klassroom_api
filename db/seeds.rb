916fda76b6ad70ff335fb77fb0c85482cf929ee9
degasm <marcel.degas@autodesk.com>
2020-12-03 08:45:58 -0800

    fixes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,77 +7,77 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Teacher.create!(first_name: Example, last_name: McExamplerson, email: example@gmail.com)
+Teacher.create!(first_name: 'Marcel', last_name: 'theTeacher', email: 'example@gmail.com')
 
 Student.create!({
-    first_name: Jerry, 
-    last_name: Garcia,
+    first_name: 'Jerry', 
+    last_name: 'Garcia',
     gender: 2,
     willingness_to_help: 2, 
     current_grade: 3
   }, {
-    first_name: Adriana, 
-    last_name: Lopez,
+    first_name: 'Adriana', 
+    last_name: 'Lopez',
     gender: 1,
     willingness_to_help: 4, 
     current_grade: 2
   }, {
-    first_name: Bahrom, 
-    last_name: Barcordar, 
+    first_name: 'Bahrom', 
+    last_name: 'Barcordar', 
     gender: 2,
     willingness_to_help: 4, 
     current_grade: 4
   }, {
-    first_name: Yessica, 
-    last_name: Lopez, 
+    first_name: 'Yessica', 
+    last_name: 'Lopez', 
     gender: 1,
     willingness_to_help: 4, 
     current_grade: 4
   }, {
-    first_name: Julie, 
-    last_name: Perkins, 
+    first_name: 'Julie', 
+    last_name: 'Perkins', 
     gender: 1,
     willingness_to_help: 4, 
     current_grade: 1
   }, {
-    first_name: Daphne, 
-    last_name: Granger, 
+    first_name: 'Daphne', 
+    last_name: 'Granger', 
     gender: 1,
     willingness_to_help: 1, 
     current_grade: 4
   }, {
-    first_name: Kevin, 
-    last_name: Barako, 
+    first_name: 'Kevin', 
+    last_name: 'Barako', 
     gender: 2,
     willingness_to_help: 1, 
     current_grade: 1
   }, {
-    first_name: Sasha, 
-    last_name: Degas, 
+    first_name: 'Sasha', 
+    last_name: 'Degas', 
     gender: 2,
     willingness_to_help: 2, 
     current_grade: 4
   }, {
-    first_name: Bronner, 
-    last_name: Pasarow, 
+    first_name: 'Bronner', 
+    last_name: 'Pasarow', 
     gender: 2,
     willingness_to_help: 4, 
     current_grade: 3
   }, {
-    first_name: Jackson,
-    last_name: Serpentine,
+    first_name: 'Jackson',
+    last_name: 'Serpentine',
     gender: 2, 
     willingness_to_help: 2, 
     current_grade: 2
   }, {
-    first_name: Benjamin, 
-    last_name: Vermon,
+    first_name: 'Benjamin', 
+    last_name: 'Vermon',
     gender: 2, 
     willingness_to_help: 3, 
     current_grade: 3
   }, {
-    first_name: Jessica, 
-    last_name: Hollywood,
+    first_name: 'Jessica', 
+    last_name: 'Sturla',
     gender: 1, 
     willingness_to_help: 4, 
     current_grade: 4
